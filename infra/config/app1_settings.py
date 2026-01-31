d53ad0b26818af8b6d93017e0272836f5fdabe90
李和谦 <418130444@qq.com>
2026-01-31 10:44:49 +0800

    update

--- infra/config/app1_settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class App1Settings(BaseSettings):
-    JWT_SECRET: str
-    WORKER_COUNT: int = 4
-
-    class Config:
-        env_prefix = "APP1_"
--- infra/config/app2_settings.py
+++
@@ -1,9 +0,0 @@
-# app2/settings.py
-from pydantic_settings import BaseSettings
-
-class App2Settings(BaseSettings):
-    API_KEY: str
-    TIMEOUT: int = 30
-
-    class Config:
-        env_prefix = "APP2_"
